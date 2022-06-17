6797f34e8815e148e5301a845274557fb8763beb
iamraufu <raufu.dev@gmail.com>
2022-06-17 21:06:23 +0600

    [UPDATED] Navbar

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -2,48 +2,78 @@ import React from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import logo from '../../../images/logo.png';
+import products from '../../../data/products.json';
+import { getStoredCart } from '../../../utilities/localDB';
 
 const Navbar = () => {
+
+    let savedCart = getStoredCart()
+    let cart = [];
+    for (let key in savedCart) {
+        cart.push({ ...products.find(pd => pd.id === key), quantity: savedCart[key] })
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg customize-navbar">
+        <nav style={{backgroundColor: '#f3f5f9'}} className="navbar navbar-expand-lg customize-navbar sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand" to="/"><img src={logo} width={50} className='img-fluid' alt="logo" />MERN E-Commerce</NavLink>
+
+                <NavLink className="navbar-brand" to="/">
+                    <img src={logo} width={50} className='img-fluid' alt="logo" />MERN E-Commerce
+                </NavLink>
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse text-center" id="navbarNavAltMarkup">
+
                     <div className="navbar-nav ms-auto">
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
+                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                             return {
                                 color: isActive ? "red" : "",
                             };
                         }} className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
+
+                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                             return {
                                 color: isActive ? "red" : "",
                             };
                         }} className="nav-link" to="/products">Products</NavLink>
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
-                            return {
-                                color: isActive ? "red" : "",
-                            };
-                        }} className="nav-link" to="/cart">Cart</NavLink>
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
+
+                        {
+                            cart.length > 0 ? <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
+                                return {
+                                    color: isActive ? "red" : "",
+                                };
+                            }} className="nav-link" to="/cart">Cart <sup className='fw-bold'>({cart.reduce((a, b) => { return a + (b.quantity); }, 0)})</sup></NavLink>
+                                :
+                                <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
+                                    return {
+                                        color: isActive ? "red" : "",
+                                    };
+                                }} className="nav-link" to="/cart">Cart</NavLink>
+                        }
+
+                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                             return {
                                 color: isActive ? "red" : "",
                             };
                         }} className="nav-link" to="/login">Login</NavLink>
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
+
+                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                             return {
                                 color: isActive ? "red" : "",
                             };
                         }} className="nav-link" to="/profile">Profile</NavLink>
-                        <NavLink onClick={()=>{window.scrollTo(0, 0);}} style={({ isActive }) => {
+
+                        <NavLink onClick={() => { window.scrollTo(0, 0); }} style={({ isActive }) => {
                             return {
                                 color: isActive ? "red" : "",
                             };
                         }} className="nav-link" to="/dashboard">Dashboard</NavLink>
+
                     </div>
+
                 </div>
             </div>
         </nav>
