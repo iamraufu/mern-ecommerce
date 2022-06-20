06e7308ca249c0578d451c43a862a66ea38d31cc
iamraufu <raufu.dev@gmail.com>
2022-06-20 15:59:19 +0600

    updated

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7rYfnFQWv0tQZpa694cqRjI4Jtf_yvdc",
+    authDomain: "mern-e-commerce-bd.firebaseapp.com",
+    projectId: "mern-e-commerce-bd",
+    storageBucket: "mern-e-commerce-bd.appspot.com",
+    messagingSenderId: "197364479335",
+    appId: "1:197364479335:web:a336cf48388258dee96bc2"
+};
+
+export default firebaseConfig;
